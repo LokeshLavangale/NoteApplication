09b23cc574be8f27ef2b7117cfe26871ea3b4e7d
Lokesh Lavangale <reach.lokesh@gmail.com>
2016-01-16 19:41:44 +0530

    Initial Check in of Note Application

---
+++ NoteApplication/src/main/java/com/lokesh/note/NoteApplication.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.lokesh.note;
+
+/**
+ * @author Lokesh Lavangale
+ *
+ */
+public class NoteApplication {
+
+	/**
+	 * 
+	 */
+	public NoteApplication() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Note Application");
+	}
+
+}

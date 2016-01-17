0b415a9a8bc439a3f7aa8a556e7fd2e854e0de27
Lokesh Lavangale <reach.lokesh@gmail.com>
2016-01-17 18:20:13 +0530

    Get webservice calls for users and notes

--- NoteApplication/src/main/java/com/lokesh/note/service/NoteService.java
+++ NoteApplication/src/main/java/com/lokesh/note/service/NoteService.java
@@ -14,6 +14,8 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 
 import com.lokesh.note.controller.NoteController;
+import com.lokesh.note.model.Note;
+import com.lokesh.note.model.Notes;
 import com.lokesh.note.model.User;
 import com.lokesh.note.model.Users;
 
@@ -54,6 +56,41 @@ public class NoteService {
 
 			User user = new NoteController().findUserById(Long.parseLong(id));
 			jaxbMarshaller.marshal(user, sw);
+		} catch (Exception e) {
+			return Response.status(200).entity(e.getMessage()).build();
+		}
+		return Response.status(200).entity(sw.toString()).build();
+	}
+	
+	@GET
+	@Path("/users/{id}/notes")
+	public Response getUserNotes(@PathParam("id") String id) {
+
+		StringWriter sw = new StringWriter();
+		try {
+			Marshaller jaxbMarshaller = JAXBContext.newInstance(Notes.class).createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			User user = new NoteController().findUserById(Long.parseLong(id));
+			jaxbMarshaller.marshal(user.getNotes(), sw);
+		} catch (Exception e) {
+			return Response.status(200).entity(e.getMessage()).build();
+		}
+		return Response.status(200).entity(sw.toString()).build();
+	}
+	
+	
+	@GET
+	@Path("/users/{id}/notes/{note_id}")
+	public Response getUserNote(@PathParam("id") String id, @PathParam("note_id") String note_id) {
+
+		StringWriter sw = new StringWriter();
+		try {
+			Marshaller jaxbMarshaller = JAXBContext.newInstance(Note.class).createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			Note note = new NoteController().findNoteById(Long.parseLong(note_id));
+			jaxbMarshaller.marshal(note, sw);
 		} catch (Exception e) {
 			return Response.status(200).entity(e.getMessage()).build();
 		}
